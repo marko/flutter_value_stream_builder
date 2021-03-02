7f173d1789f870b0da5bdcbe4ce9d787458f6773
Filip Marko <filip.lindman.marko@gmail.com>
2021-03-02 01:59:59 +0100

    Initial commit

---
+++ lib/flutter_value_stream_builder.dart
@@ -0,0 +1,17 @@
+library flutter_value_stream_builder;
+
+import 'package:flutter/widgets.dart';
+import 'package:rxdart/streams.dart';
+
+class ValueStreamBuilder<T> extends StreamBuilder<T> {
+  ValueStreamBuilder({
+    Key key,
+    @required ValueStream<T> valueStream,
+    @required AsyncWidgetBuilder<T> builder,
+  }) : super(
+          key: key,
+          initialData: valueStream.value,
+          stream: valueStream,
+          builder: builder,
+        );
+}
